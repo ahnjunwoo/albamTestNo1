f747d2fbe176a91f8a5993695d5661f4d2e4fcb7
DESKTOP-2CNQ44B\ahnjunwoo <cutesboy2@gmail.com>
2018-02-26 19:47:37 +0900

    boot 최초 구성

---
+++ src/main/java/com/albam/AlbamTestNo1Application.java
@@ -0,0 +1,12 @@
+package com.albam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbamTestNo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbamTestNo1Application.class, args);
+	}
+}
